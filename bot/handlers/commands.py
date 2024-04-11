13efe078cbbecb4411a61fae35d994e4e62cb5ef
Vladimir Kul'kov <kulkov.vk@dns-shop.ru>
2024-04-11 22:28:02 +1000

    Revert "айди чата в терминеал"
    This reverts commit a826c7e1a2d2c518e573536ac7c1350638f39029.

--- bot/handlers/commands.py
+++ bot/handlers/commands.py
@@ -8,8 +8,7 @@ from settings import RESULT_CHAT_ID, ADDITIONAL_RESULT_CHAT_ID
 
 @dp.message_handler(CommandStart(), chat_type=[types.ChatType.GROUP, types.ChatType.SUPERGROUP])
 async def start_user(message: types.Message):
-    if message.chat.id not in [RESULT_CHAT_ID, ADDITIONAL_RESULT_CHAT_ID]:
-        print(f"Wrong chat: {message.chat.id}")
+    if message.chat.id != RESULT_CHAT_ID:
         return
     await message.answer("Статистика по местам в поиске доступна по команде /search")
 
